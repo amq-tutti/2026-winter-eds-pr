fc954ac0834a4bf10736ac91fb6801f058420f8d
amq-tutti <tutti-amq@proton.me>
2026-03-08 14:52:33 +0200

    Update title and description in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
     localStoragePrefix: "2026-winter-eds-pr",
-    title: "Party Rank Sorter",
-    description: "Party rank sorter for your custom list of songs."
+    title: "2026 Winter EDs",
+    description: "Party rank sorter for anime EDs from 2026 winter season."
 };
